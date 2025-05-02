faacf3ddd2a85d8b3acf511f6d6a626f46d5053f
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-05-02 08:10:30 +0000

    latest commit

---
+++ src/Spinach.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Spinach = () => {
+  return (
+    <>
+      <p>Hi, I am Spinach! Popeye loves to eat me!</p>
+      <Link to="/">Click here to go back</Link>
+    </>
+  );
+};
+
+export default Spinach;
+    
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Profile from "./Profile";
+
+const router = createBrowserRouter([
+
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "profile/:name",
+    element: <Profile />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
